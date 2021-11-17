66641174226360f557fa03717db837c49778c9fe
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-11-17 19:50:46 +0100

    Define interfaces

--- contracts/wynd-faucet/src/error.rs
+++ contracts/wynd-faucet/src/error.rs
@@ -6,8 +6,6 @@ pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
 
-    #[error("Unauthorized")]
-    Unauthorized {},
-    // Add any other custom errors you like here.
-    // Look at https://docs.rs/thiserror/1.0.21/thiserror/ for details.
+    #[error("Used up all calls: {0}")]
+    UsedAllCalls(u32),
 }
--- contracts/wynd-faucet/src/state.rs
+++ contracts/wynd-faucet/src/state.rs
@@ -1,13 +1,18 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cosmwasm_std::{Addr, Uint128};
+use cw_storage_plus::{Item, Map};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: Addr,
+pub struct Config {
+    /// which cw20 token we send
+    pub token: Addr,
+    /// how much we send on request
+    pub amount: Uint128,
+    /// max times we pay out for a given account, default 1
+    pub max_requests: u32,
 }
 
-pub const STATE: Item<State> = Item::new("state");
+pub const CONFIG: Item<Config> = Item::new("config");
+pub const CALLS: Map<&Addr, u32> = Map::new("calls");
